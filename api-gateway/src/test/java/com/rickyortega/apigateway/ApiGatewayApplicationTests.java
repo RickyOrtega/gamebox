bf572869e60b1d155917273fe10e2688a574fa8d
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2024-08-19 08:29:19 -0500

    Initial commit

---
+++ api-gateway/src/test/java/com/rickyortega/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickyortega.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ eureka-server/src/test/java/com/rickyortega/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickyortega.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
